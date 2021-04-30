268c3f88903cc592394d1247c0bedca8222c713a
K.Kiyokawa <koichi20110068@gmail.com>
2021-04-30 18:35:52 +0900

    chore: jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,10 @@
-export default {
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
   preset: "ts-jest",
   coverageDirectory: "coverage",
   coverageProvider: "v8",
   testEnvironment: "node",
 };
+
+export default config;
