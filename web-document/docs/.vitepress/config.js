a1d2d5b6edf89f2dfff3b3ba7afd58201b14cf80
K.Kiyokawa <koichi20110068@gmail.com>
2021-04-30 22:49:25 +0900

    docs: add GitHub link

--- web-document/docs/.vitepress/config.js
+++ web-document/docs/.vitepress/config.js
@@ -1,4 +1,11 @@
 module.exports = {
   title: 'rhodash',
   description: 'Just playing around.',
+  themeConfig: {
+    repo: 'KoichiKiyokawa/rhodash',
+    docsDir: 'web-document/docs', // has bug, branch name cannot set as `main1`
+
+    editLinks: true,
+    editLinkText: 'Edit this page on GitHub',
+  },
 }
