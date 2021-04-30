4d6db135229edb3f116bdccb14ee1d7752869ccf
K.Kiyokawa <koichi20110068@gmail.com>
2021-04-30 18:41:17 +0900

    feat: support cjs

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,15 @@ import typescript from "@rollup/plugin-typescript";
 /** @type {import('rollup').RollupOptions} */
 const options = {
   input: "src/index.ts",
-  output: {
-    dir: "dist",
-    format: "es",
-    exports: "named",
-    sourcemap: true,
-  },
+  output: [
+    { file: "dist/index.cjs", format: "cjs" },
+    {
+      dir: "dist",
+      format: "es",
+      exports: "named",
+      sourcemap: true,
+    },
+  ],
 
   plugins: [
     typescript({
