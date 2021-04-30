4ace80246bfeff1cdb57e69406984686f53a0730
K.Kiyokawa <koichi20110068@gmail.com>
2021-04-30 19:20:11 +0900

    fix: sum type def

--- src/sum.ts
+++ src/sum.ts
@@ -1,7 +1,7 @@
 /**
  * @example sum([1, 2, 3]) // 6
  */
-export function sum(numbers: number[]): number {
+export function sum(numbers: readonly number[]): number {
   let result = 0
   for (const num of numbers) result += num
   return result
