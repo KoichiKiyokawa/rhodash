35c3c8e624abdea5c97ffc1fde326954276b74db
K.Kiyokawa <koichi20110068@gmail.com>
2021-04-30 10:14:18 +0900

    refactor: delay test

--- test/delay.spec.ts
+++ test/delay.spec.ts
@@ -1,11 +1,17 @@
 import { delay } from "../src/index";
 
 describe("default use case", () => {
-  it("delay 3000ms", async () => {
+  it("delay 1000ms", async () => {
+    const waitTime = 1000;
+
     const startUnixTime = new Date().getTime();
-    await delay(3000);
+    await delay(waitTime);
     const endUnixTime = new Date().getTime();
-    expect(endUnixTime - startUnixTime).toBeLessThan(3500);
-    expect(endUnixTime - startUnixTime).toBeGreaterThan(2500);
+
+    const elapsedTime = endUnixTime - startUnixTime;
+    const allowableError = 500;
+
+    expect(elapsedTime).toBeLessThan(waitTime + allowableError);
+    expect(elapsedTime).toBeGreaterThan(waitTime - allowableError);
   });
 });
