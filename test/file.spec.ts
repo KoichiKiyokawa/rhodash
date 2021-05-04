43b8f49415be44b878a7c304abf70fc9831f54ae
K.Kiyokawa <koichi20110068@gmail.com>
2021-05-04 14:09:16 +0900

    test: check if function has example

--- test/file.spec.ts
+++ test/file.spec.ts
@@ -6,19 +6,35 @@ describe('file pattern', () => {
     const ok = await isAllTestFileValid()
     expect(ok).toBe(true)
   })
+
+  it('test all function file has example', async () => {
+    const ok = await isAllFunctionHasExample()
+    expect(ok).toBe(true)
+  })
 })
 
 async function isAllTestFileValid() {
   const files = await promisify(fs.readdir)('./test')
   for (const file of files) {
     // check if test file is *.spec.ts
-    if (!file.endsWith('spec.ts')) return 'invalid file extension: ' + file
+    if (!file.endsWith('spec.ts')) return `invalid file extension: ${file}`
     const text = String(fs.readFileSync(`./test/${file}`))
 
     if (file === 'file.spec.ts') continue
     // check if test file has import { foo } from '../src/index', not 'import foo from '../src/foo'.
-    if (!/import { \w+ } from '\.\.\/src\/index'/.test(text)) return 'invalid import: ' + file
+    if (!/import { \w+ } from '\.\.\/src\/index'/.test(text)) return `invalid import: ${file}`
   }
 
+  return true
+}
+
+async function isAllFunctionHasExample() {
+  const files = await promisify(fs.readdir)('./src')
+  for (const file of files) {
+    if (file === 'index.ts') continue
+
+    const text = String(fs.readFileSync(`./src/${file}`))
+    if (!/@example/.test(text)) return `lack of example: ${file}`
+  }
   return true
 }
