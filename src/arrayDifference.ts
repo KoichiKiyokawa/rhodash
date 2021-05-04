082bf01700507b329d9e04a21b1d0026d9446ea6
K.Kiyokawa <koichi20110068@gmail.com>
2021-05-04 14:09:41 +0900

    chore: add missing example

--- src/arrayDifference.ts
+++ src/arrayDifference.ts
@@ -4,6 +4,7 @@
  * @param target target array
  * @param other other array
  * @returns an array that contains values not included in the other given array, using `===` string equal.
+ * @example arrayDifference([1, 2, 3], [2, 3]) // [1]
  */
 export function arrayDifference<T>(target: readonly T[], other: readonly T[]): T[] {
   return target.filter((item) => !other.includes(item))
--- src/sumBy.ts
+++ src/sumBy.ts
@@ -1,3 +1,13 @@
+/**
+ * @example
+ * const users = [
+ *    { age: 1, name: 'user1' },
+ *    { age: 2, name: 'user2' },
+ *    { age: 3, name: 'user3' },
+ * ]
+ * sumBy(users, user => user.age) // 6
+ * sumBy(users, 'age') // 6
+ */
 export function sumBy<T extends Record<string, unknown>>(
   array: readonly T[],
   predicate: ((arg: T) => number) | { [K in keyof T]: T[K] extends number ? K : never }[keyof T]
