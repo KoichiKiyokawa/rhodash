5de245529b4c6ccdce27b9905d1f468225201496
K.Kiyokawa <koichi20110068@gmail.com>
2021-06-24 23:47:24 +0900

    refactor: new_version script

--- scripts/new_version.ts
+++ scripts/new_version.ts
@@ -1,37 +1,38 @@
 import 'zx'
-;(async function () {
-  const error = (msg: string) => {
-    console.error(chalk.red(msg))
-    process.exit(1)
-  }
 
-  // prettier-ignore
-  const [/* node */, /* zx */, /* file */, newVersion, ...others] = process.argv
-  if (others.length > 0) error('too much args')
-  if (newVersion == null) error('please input new version. e.g. 1.0.0')
+const error = (msg: string) => {
+  console.error(chalk.red(msg))
+  process.exit(1)
+}
 
-  // update package.json
-  const pkg = JSON.parse((await fs.readFile('package.json')).toString())
-  pkg.version = newVersion
-  await fs.writeFile('package.json', JSON.stringify(pkg))
-  await $`npx prettier --write package.json`
+// prettier-ignore
+const [/* node */, /* zx */, /* file */, newVersion, ...others] = process.argv
+if (others.length > 0) error('too much args')
+if (newVersion == null) error('please input new version. e.g. 1.0.0')
 
-  // update readme
-  const readmeRes = await fs.readFile('README.md')
-  const newReadme = readmeRes
-    .toString()
-    .replace(
-      new RegExp('"https://unpkg.com/rhodash@.*?"'),
-      `"https://unpkg.com/rhodash@${newVersion}"`
-    )
-  await fs.writeFile('README.md', newReadme)
+// update package.json
+const pkg = JSON.parse((await fs.readFile('package.json')).toString())
+pkg.version = newVersion
+await fs.writeFile('package.json', JSON.stringify(pkg))
+await $`npx prettier --write package.json`
 
-  // git tag
-  await $`npm install`
-  await $`npm run build`
-  await $`git add -A`
-  await $`git commit -m "v${newVersion}"`
-  await $`git tag "v${newVersion}" HEAD -f`
+// update readme
+const readmeRes = await fs.readFile('README.md')
+const newReadme = readmeRes
+  .toString()
+  .replace(
+    new RegExp('"https://unpkg.com/rhodash@.*?"'),
+    `"https://unpkg.com/rhodash@${newVersion}"`
+  )
+await fs.writeFile('README.md', newReadme)
 
-  console.log(`push by below command:\n ${chalk.green('$ git push origin --tags && npm publish')}`)
-})()
+// git tag
+await $`npm install`
+await $`npm run build`
+await $`git add -A`
+await $`git commit -m "v${newVersion}"`
+await $`git tag "v${newVersion}" HEAD -f`
+
+console.log(
+  `push by below command:\n ${chalk.green('$ git push -u origin HEAD --tags && npm publish')}`
+)
