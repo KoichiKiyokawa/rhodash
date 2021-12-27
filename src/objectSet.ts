dbeafbf75af5c4c801fdaf3d6e09fb7d697546f6
K.Kiyokawa <koichi20110068@gmail.com>
2021-12-28 00:20:49 +0900

    test: add

--- src/objectSet.ts
+++ src/objectSet.ts
@@ -7,12 +7,19 @@
  * @example
  * objectSet({}, 'a.b.c[0]', 1) // => { a: { b: { c: [1] } } }
  * objectSet({}, 'a.b.c.0', 1) // => { a: { b: { c: [1] } } }
+ * // merge objects
+ * objectSet({ a: 1 }, 'a.b', 2) // => { a: { b: 2 } }
  */
 export function objectSet<T>(obj: any, path: string, value: unknown): T {
   const keys = path.split(/\[|\]|\./).filter(Boolean)
   let current = obj
   for (let i = 0; i < keys.length; i++) {
     const key = isEmptyQuote(keys[i]) ? '' : keys[i]
+    // for merge objects like: objectSet({ a: 1 }, 'a.b', 2) eq { a: { b: 2 } }
+    if (!isObjectOrArrayLike(current[key])) {
+      current[key] = isNumberString(keys[i + 1]) ? [] : {}
+    }
+
     if (i !== keys.length - 1) {
       current = current[key] ??= isNumberString(keys[i + 1]) ? [] : {}
     } else {
@@ -28,4 +35,8 @@ function isNumberString(str: string): boolean {
 
 function isEmptyQuote(str: string): boolean {
   return str === '""' || str === "''"
+}
+
+function isObjectOrArrayLike(obj: unknown): boolean {
+  return obj !== null && typeof obj === 'object'
 }
--- test/objectSet.spec.ts
+++ test/objectSet.spec.ts
@@ -1,21 +1,31 @@
 import { objectSet } from '../src/index'
 
 describe('default use case', () => {
-  it(`objectSet({}, 'a.b.c[0]', 1) is { a: { b: { c: [1] } } }`, () => {
-    expect(objectSet({}, 'a.b.c[0]', 1)).toEqual({ a: { b: { c: [1] } } })
-  })
-  it(`objectSet({}, 'a.b.c.0', 1) is { a: { b: { c: [1] } } }`, () => {
-    expect(objectSet({}, 'a.b.c.0', 1)).toEqual({ a: { b: { c: [1] } } })
-  })
-  it(`objectSet({}, 'a.b.c0', 1) is { a: { b: { c0: 1 } } }`, () => {
-    expect(objectSet({}, 'a.b.c0', 1)).toEqual({ a: { b: { c0: 1 } } })
-  })
-  it(`objectSet({}, 'a.b.0c', 1) is { a: { b: { "0c": 1 } } }`, () => {
-    expect(objectSet({}, 'a.b.0c', 1)).toEqual({ a: { b: { '0c': 1 } } })
+  describe('assign to an empty object or array', () => {
+    it(`objectSet({}, 'a.b.c[0]', 1) is { a: { b: { c: [1] } } }`, () => {
+      expect(objectSet({}, 'a.b.c[0]', 1)).toEqual({ a: { b: { c: [1] } } })
+    })
+    it(`objectSet({}, 'a.b.c.0', 1) is { a: { b: { c: [1] } } }`, () => {
+      expect(objectSet({}, 'a.b.c.0', 1)).toEqual({ a: { b: { c: [1] } } })
+    })
+    it(`objectSet({}, 'a.b.c0', 1) is { a: { b: { c0: 1 } } }`, () => {
+      expect(objectSet({}, 'a.b.c0', 1)).toEqual({ a: { b: { c0: 1 } } })
+    })
+    it(`objectSet({}, 'a.b.0c', 1) is { a: { b: { "0c": 1 } } }`, () => {
+      expect(objectSet({}, 'a.b.0c', 1)).toEqual({ a: { b: { '0c': 1 } } })
+    })
   })
 
-  it(`objectSet({ a: 1 }, 'a.b', 2) is { a: { b: 2 } }`, () => {
-    expect(objectSet({ a: 1 }, 'a.b', 2)).toEqual({ a: { b: 2 } })
+  describe('merge objects', () => {
+    it(`objectSet({ a: 1 }, 'a', 2) is { a: 2 }`, () => {
+      expect(objectSet({ a: 1 }, 'a.b', 2)).toEqual({ a: { b: 2 } })
+    })
+    it(`objectSet({ a: 1 }, 'a.b', 2) is { a: { b: 2 } }`, () => {
+      expect(objectSet({ a: 1 }, 'a.b', 2)).toEqual({ a: { b: 2 } })
+    })
+    it(`objectSet({ a: [{ b: 1 }] }, 'a.0.b.c', 2) is { a: [{ b: { c: 2 } }] }`, () => {
+      expect(objectSet({ a: [{ b: 1 }] }, 'a.0.b.c', 2)).toEqual({ a: [{ b: { c: 2 } }] })
+    })
   })
 })
 
@@ -27,5 +37,11 @@ describe('edge case', () => {
   })
   it('path is an empty string', () => {
     expect(objectSet({}, '', 1)).toEqual({})
+  })
+  it(`cannot set object path to an array`, () => {
+    const result: any = objectSet([], 'a.b', 1)
+    expect(Array.isArray(result)).toBe(true)
+    expect(result['a']).toEqual({ b: 1 })
+    expect(result['a']['b']).toEqual(1)
   })
 })
