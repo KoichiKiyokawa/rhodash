76754190c2d8ef847679961a7d3f375a41f3976c
K.Kiyokawa <koichi20110068@gmail.com>
2022-02-07 16:49:52 +0900

    wip

--- src/cloneDeep.ts
+++ src/cloneDeep.ts
@@ -10,8 +10,10 @@ export function cloneDeep<T>(value: T): T {
   if (!value) return value
   if (Array.isArray(value)) return value.map(cloneDeep) as unknown as T
   if (value instanceof Date) return new Date(value) as unknown as T
-  if (value instanceof Map) return new Map(value.entries()) as unknown as T
-  if (value instanceof Set) return new Set(value.entries()) as unknown as T
+  if (value instanceof Map)
+    return new Map(Array.from(value.entries()).map(([k, v]) => [k, cloneDeep(v)])) as unknown as T
+  if (value instanceof Set)
+    return new Set(Array.from(value.entries()).map(([k, v]) => [k, cloneDeep(v)])) as unknown as T
   if (typeof value === 'object') {
     const copied = {} as T
     for (const key in value) copied[key] = cloneDeep(value[key])
