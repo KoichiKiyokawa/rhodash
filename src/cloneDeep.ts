2b39362a7c12c9d28a99bb6f6d4601139aa7e7c3
K.Kiyokawa <koichi20110068@gmail.com>
2022-02-07 17:28:15 +0900

    comment

--- src/cloneDeep.ts
+++ src/cloneDeep.ts
@@ -25,6 +25,7 @@ export function cloneDeep<T>(value: T): T {
     for (const v of value.values()) copied.add(cloneDeep(v))
     return copied as unknown as T
   }
+  // TODO: buffer
   if (typeof value === 'object') {
     const copied = {} as T
     for (const key in value) copied[key] = cloneDeep(value[key])
